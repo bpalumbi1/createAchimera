82f91540291751968ffbfe59670ff02729ae5276
bpalumbi <bellapalumbi@Bellas-MacBook-Air.local>
2015-10-17 12:29:20 -0400

    Added createAchimera
    Added all createAchimera files from the completed Hackathon game.

---
+++ main.lua
@@ -0,0 +1,6 @@
+--Bella Palumbi
+--createAchimera
+
+local storyboard=require("storyboard")
+local transitionOptions={effect="fade",time=400}
+storyboard.gotoScene("QuestionScene1",transitionOptions)
